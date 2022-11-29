9f83068d872d16c49035fdca128dc8b6c48da600
bartj2341 <bartefix@gmail.com>
2022-11-29 18:03:44 +0100

    FEAT: Recoil Modal State (skeleton)

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@chakra-ui/react';
 import React from 'react';
-
+import { useSetRecoilState } from 'recoil';
+import { authModalState } from '../../../atoms/authModalAtom';
 
 const AuthButtons: React.FC = () => {
+    const setAuthModalState = useSetRecoilState(authModalState)
 
     return (
         <>
@@ -12,16 +14,16 @@ const AuthButtons: React.FC = () => {
                 display={{ base: 'none', sm: 'flex' }}
                 width={{ base: '70px', md: "110px" }}
                 mr={2}
-                // onClick={() => {}}
+                onClick={() => setAuthModalState( {open: true, view: "login" })}
             >
                 Log In
             </Button>
             <Button
                 height="28px"
-                display={{ base: 'none', sm: 'flex' }}
+                display={{ base: 'none', sm: 'flex' }} 
                 width={{ base: '70px', md: "110px" }}
                 mr={2}
-                // onClick={() => { }}
+                onClick={() => setAuthModalState({ open: true, view: "signup" })}
             >
                 Sign Up
             </Button>
