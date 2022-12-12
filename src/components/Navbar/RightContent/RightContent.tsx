e8e5abea94cd152054fccba2ee384ca963f644b4
bartj2341 <bartefix@gmail.com>
2022-12-12 16:15:39 +0100

    FEAT: User Dropdown Menu

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,11 +1,13 @@
 import { Button, Flex } from '@chakra-ui/react';
-import { signOut } from 'firebase/auth';
+import { signOut, User } from 'firebase/auth';
 import React from 'react';
 import { auth } from '../../../firebase/clientApp';
 import AuthButtons from './AuthButtons';
+import Icons from './Icons';
+import UserMenu from './UserMenu';
 
 type RightContentProps = {
-    user: any;
+    user?: User | null;
 };
 
 const RightContent:React.FC<RightContentProps> = ({ user }) => {
@@ -14,7 +16,8 @@ const RightContent:React.FC<RightContentProps> = ({ user }) => {
         <>
         {/* <AuthModal/> */}
         <Flex justify='center' align='center'>
-            {user ? <Button onClick={() => signOut(auth)}>Logout</Button> : <AuthButtons/>}
+            {user ? <Icons/> : <AuthButtons/>}
+            <UserMenu user={user}/>
         </Flex>
         </>
     )
