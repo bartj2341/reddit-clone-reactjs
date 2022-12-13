32bc1d35b744cce45d3ab62c27b030db83bab244
bartj2341 <bartefix@gmail.com>
2022-12-13 13:48:04 +0100

    FEAT: Create Community Modal

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,17 +5,18 @@ import SearchInput from './SearchInput';
 import AuthModal from '../Modal/Auth/AuthModal'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase/clientApp';
+import Directory from './Directory/Directory';
 
 const Navbar: React.FC = () => {
     const [user, loading, error] = useAuthState(auth)
     return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex align="center">
+        <Flex bg="white" height="44px" padding="6px 12px" justify={{md: 'space-between'}}>
+            <Flex align="center" width={{base: '40px', md: "auto"}} mr={{base: 0, md: 2}}>
                 <Image src="/images/redditFace.svg" height="30px" alt="redditFace" />
                 <Image src="/images/redditText.svg" height="46px" alt="redditText" display={{ base: "none", md: "unset" }} />
             </Flex>
-            {/* <Directory/> */}
-            <SearchInput/>
+            {user && <Directory/>}
+            <SearchInput user={user}/>
             <RightContent user={user}/>
             <AuthModal/>
         </Flex>
