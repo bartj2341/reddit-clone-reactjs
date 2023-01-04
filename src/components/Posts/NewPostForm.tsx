84c992e314423d0be418459ccad42520c544dab4
bartj2341 <bartefix@gmail.com>
2023-01-04 19:52:19 +0100

    FEAT: Home Feed for Logged-Out Users

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -17,6 +17,7 @@ import TabItem from "./TabItem";
 
 type NewPostFormProps = {
 	user: User;
+	communityImageURL?: string;
 };
 
 const formTabs: TabObject[] = [
@@ -47,7 +48,7 @@ export type TabObject = {
 	icon: typeof Icon.arguments;
 };
 
-const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+const NewPostForm: React.FC<NewPostFormProps> = ({ user, communityImageURL }) => {
 	const router = useRouter();
 
 	const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
@@ -66,6 +67,7 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
 		// create new post object => type Post
 		const newPost: Post = {
 			communityId: communityId as string,
+			communityImageURL: communityImageURL || '',
 			creatorId: user.uid,
 			creatorDisplayName: user.email!.split('@')[0],
 			title: textInputs.title,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,86 @@
-export default function Home() {
+import { Stack } from "@chakra-ui/react";
+import { query, collection, orderBy, limit, getDocs } from "firebase/firestore";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Post } from "../atoms/postsAtom";
+import CreatePostLink from "../components/Community/CreatePostLink";
+import PageContent from "../components/Layout/PageContent";
+import PostItem from "../components/Posts/PostItem";
+import PostLoader from "../components/Posts/PostLoader";
+import { auth, firestore } from "../firebase/clientApp";
+import usePosts from "../hooks/usePosts";
+
+const Home: NextPage = () => {
+	const [user, loadingUser] = useAuthState(auth)
+	const [loading, setLoading] = useState(false);
+	const { postsStateValue, setPostsStateValue, onSelectPost, onDeletePost, onVote } = usePosts();
+	
+	const buildUserHomeFeed = () => {
+		// fetch some posts from community that the user is in
+	};
+
+	const buildNoUserHomeFeed = async() => {
+		setLoading(true);
+		try {
+		const postQuery = query(
+			collection(firestore, "posts"),
+			orderBy("voteStatus", "desc"),
+			limit(10)
+		);
+
+		const postDocs = await getDocs(postQuery);
+		const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+		// setPostState
+		setPostsStateValue((prev) => ({
+			...prev,
+			posts: posts as Post[],
+		}));
+
+		
+		} catch (error) {
+		console.log("buildNoUserHomeFeed error", error);
+		}
+		setLoading(false);
+	};
+
+	const getUserPostVotes = () => {};
+
+	// useEffects
+	useEffect(() => {
+		if(!user && !loadingUser) buildNoUserHomeFeed();
+
+	}, [user, loadingUser])
+
+
 	return (
-		<div>Hello</div>
+		<PageContent>
+			<>
+				<CreatePostLink/>
+				{loading ? (
+					<PostLoader/>
+				) : (
+					<Stack>
+						{postsStateValue.posts.map(post => (
+							<PostItem
+								key={post.id}
+								post={post}
+								userIsCreator={user?.uid === post.creatorId}
+								userVoteValue={postsStateValue.postVotes.find(vote => vote.postId === post.id)?.voteValue}
+								onVote={onVote}
+								onSelectPost={onSelectPost}
+								onDeletePost={onDeletePost}
+								homePage
+							/>
+						))}	
+					</Stack>
+				)}
+			</>
+			<>
+				{/* <Recommendations/> */}
+			</>
+		</PageContent>
 	)
 }
+
+export default Home;
